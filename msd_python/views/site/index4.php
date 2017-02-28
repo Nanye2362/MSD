7f951cc41c1356d73d11f3dc993a64ba07511e9a
nanye <nanye2362@163.com>
2017-02-28 17:45:48 +0800

    timeline get diff

--- msd_python/views/site/index4.php
+++ msd_python/views/site/index4.php
@@ -1,103 +1,82 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title></title>
-    <link rel="stylesheet" type="text/css" href="../css/reset.css"/>
-    <link rel="stylesheet" type="text/css" href="../css/index4.css"/>
-</head>
-<body>
-<?php include "menu.php";?>
-<div id="head">
-    <div class="clearfloat head">
-        <div class="fl">
-            <span>注射用区拖住单抗</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <span>fieldof‘Certificat</span>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="../css/reset.css"/>
+        <link rel="stylesheet" type="text/css" href="../css/index4.css"/>
+    </head>
+    <body>
+        <?php include "menu.php"; ?>
+        <div id="head">
+            <div class="clearfloat head">
+                <div class="fl">
+                    <span>注射用区拖住单抗</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <span>fieldof‘Certificat</span>
+                </div>
+                <div class="fr" style="font-size:14px ;">
+                    上海罗氏制药有限公司
+                </div>
+            </div>
         </div>
-        <div class="fr" style="font-size:14px ;">
-            上海罗氏制药有限公司
+        <div id="cont">
+            <div class="cont_a hk">
+                <ul><li class="nth_0"></li></ul>
+                <ul id="tagposition">
+                    <li id="cont_a_nth_1" class="nth_1">The field of ‘Certificate</li>
+                    <li id="cont_a_nth_2" class="nth_2">Thield of ‘Certificate</li>
+                    <li id="cont_a_nth_3" class="nth_3">The field o</li>
+                    <li id="cont_a_nth_4" class="nth_4">Thf ‘Certificate</li>
+                    <li id="cont_a_nth_5" class="nth_5">The fieldertificate</li>
+                </ul>
+            </div><div class="cont_b hk">
+                <ul class="ul_one">
+                    <li>
+                        <ul class="ul_0 nth_0"></ul>
+                    </li>
+                    <div id="timelinepositon">
+                        <li id="timelinepositon_1"><ul class="ul_1 nth_1"></ul></li>
+                        <li id="timelinepositon_2"><ul class="ul_1 nth_2"></ul></li>
+                        <li id="timelinepositon_3"><ul class="ul_1 nth_3"></ul></li>
+                        <li id="timelinepositon_4"><ul class="ul_1 nth_4"></ul></li>
+                        <li id="timelinepositon_5"><ul class="ul_1 nth_5"></ul></li>                        
+                    </div>
+                    <li>
+                        <ul class="ul_c">
+
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-</div>
-<div id="cont">
-    <div class="cont_a hk">
-        <ul>
-            <li class="nth_1">The field of ‘Certificate</li>
-            <li class="nth_2">Thield of ‘Certificate</li>
-            <li class="nth_3">The field o</li>
-            <li class="nth_4">Thf ‘Certificate</li>
-            <li class="nth_5">The fieldertificate</li>
-        </ul>
-    </div><div class="cont_b hk">
-        <ul class="ul_one">
-            <li>
-                <!--                    	<div class="date date_a">2017-01-01</div>
-                                        <div class="xuxian xian_a"></div>-->
-                <ul class="ul_1 nth_1">
-
-                </ul>
-            </li>
-            <li>
-                <!--                    	<div class="date date_b">2017-01-01</div>
-                                        <div class="xuxian xian_b"></div>-->
-                <ul class="ul_1 nth_2">
-
-                </ul>
-            </li>
-            <li>
-                <!--                    	<div class="date date_c">2017-01-01</div>
-                                        <div class="xuxian xian_c"></div>-->
-                <ul class="ul_1 nth_3">
-
-                </ul>
-            </li>
-            <li>
-                <!--                    	<div class="date date_d">2017-01-01</div>
-                                        <div class="xuxian xian_d"></div>-->
-                <ul class="ul_1 nth_4">
-
-                </ul>
-            </li>
-            <li>
-                <!--                    	<div class="date date_e">2017-01-01</div>
-                                        <div class="xuxian xian_e"></div>-->
-                <ul class="ul_1 nth_5">
-
-                </ul>
-            </li>
-            <li>
-                <ul class="ul_c">
-
-                </ul>
-            </li>
-        </ul>
-    </div>
-</div>
-<script src="../js/jquery-1.9.1.js" type="text/javascript" charset="utf-8"></script>
-<script src="../js/setting.js" type="text/javascript" charset="utf-8"></script>
+        <script src="../js/jquery-1.9.1.js" type="text/javascript" charset="utf-8"></script>
+        <script src="../js/setting.js" type="text/javascript" charset="utf-8"></script>
 
 
-<script type="text/javascript">
-    function GetQueryString(name)
-    {
-        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-        var r = window.location.search.substr(1).match(reg);
-        if (r != null)
-            return  unescape(r[2]);
-        return null;
-    }
+        <script type="text/javascript">
+            function GetQueryString(name)
+            {
+                var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+                var r = window.location.search.substr(1).match(reg);
+                if (r != null)
+                    return  unescape(r[2]);
+                return null;
+            }
 
 
-    $(function () {
+            $(function () {
                 var code = GetQueryString('code');
                 if (code.length > 0) {
                     $.post(host + 'python/gettimeline', {'cdeId': code}, function (data) {
                         var totalWeek = 0;//总周数
                         var yuweek = 0;
                         console.log(data.data.timeline);
-                        console.log(data.data.end_date);
-
+                        console.log(data);
+                        
+                        var li_num = new Array;
+                        
                         for (var i in data.data.timeline) {
+                            li_num.push(i);
                             console.log(i);
                             //填充空白展位
                             for (var j = 0; j < parseInt(totalWeek); j++) {
@@ -153,35 +132,39 @@
 
                             var enddate = data.data.end_date[i];
                             if (i == 0) {
-                                $('.ul_one').append("<div class='fxuxian' style='position: absolute;top: 0;left: 0;height: 100%;border-right: 1px dotted #ccc;'></div>");
-                                $('.ul_one').append("<div class='fdate' style='position: absolute;top: -1px;width: 50px;padding-top: 3px;'>" + enddate + "</div>");
+                                $('.ul_one').append("<div class='fxuxian' style='position: absolute;top: 32px;left: 0;height: 160px;border-right: 1px dotted rgb(138, 131, 131);'></div>");
+                                $('.ul_one').append("<div class='fdate' style='position: absolute;top: -1px;padding-top: 15px;width: 56px;'>" + enddate + "</div>");
                                 $('.fxuxian').css('width', divwidth);
-                                $('.fdate').css('left', divwidth);
+                                $('.fdate').css('left', divwidth - 19);
                             } else if (i == 1) {
-                                $('.ul_one').append("<div class='sxuxian' style='position: absolute;top: 0;left: 0;height: 100%;border-right: 1px dotted #ccc;'></div>");
-                                $('.ul_one').append("<div class='sdate' style='position: absolute;top: -1px;width: 50px;padding-top: 3px;'>" + enddate + "</div>");
+                                $('.ul_one').append("<div class='sxuxian' style='position: absolute;top: 32px;left: 0;height: 160px;border-right: 1px dotted rgb(138, 131, 131);'></div>");
+                                $('.ul_one').append("<div class='sdate' style='position: absolute;top: -1px;padding-top: 15px;width: 56px;'>" + enddate + "</div>");
                                 $('.sxuxian').css('width', divwidth);
-                                $('.sdate').css('left', divwidth);
+                                $('.sdate').css('left', divwidth - 19);
                             } else if (i == 2) {
-                                $('.ul_one').append("<div class='txuxian' style='position: absolute;top: 0;left: 0;height: 100%;border-right: 1px dotted #ccc;'></div>");
-                                $('.ul_one').append("<div class='tdate' style='position: absolute;top: -1px;width: 50px;padding-top: 3px;'>" + enddate + "</div>");
+                                $('.ul_one').append("<div class='txuxian' style='position: absolute;top: 32px;left: 0;height: 160px;border-right: 1px dotted rgb(138, 131, 131);'></div>");
+                                $('.ul_one').append("<div class='tdate' style='position: absolute;top: -1px;padding-top: 15px;width: 56px;'>" + enddate + "</div>");
                                 $('.txuxian').css('width', divwidth);
-                                $('.tdate').css('left', divwidth);
+                                $('.tdate').css('left', divwidth - 19);
                             } else if (i == 3) {
-                                $('.ul_one').append("<div class='foxuxian' style='position: absolute;top: 0;left: 0;height: 100%;border-right: 1px dotted #ccc;'></div>");
-                                $('.ul_one').append("<div class='fodate' style='position: absolute;top: -1px;width: 50px;padding-top: 3px;'>" + enddate + "</div>");
+                                $('.ul_one').append("<div class='foxuxian' style='position: absolute;top: 32px;left: 0;height: 160px;border-right: 1px dotted rgb(138, 131, 131);'></div>");
+                                $('.ul_one').append("<div class='fodate' style='position: absolute;top: -1px;padding-top: 15px;width: 56px;'>" + enddate + "</div>");
                                 $('.foxuxian').css('width', divwidth);
-                                $('.fodate').css('left', divwidth);
+                                $('.fodate').css('left', divwidth - 19);
                             } else if (i == 4) {
-                                $('.ul_one').append("<div class='fixuxian' style='position: absolute;top: 0;left: 0;height: 100%;border-right: 1px dotted #ccc;'></div>");
-                                $('.ul_one').append("<div class='fidate' style='position: absolute;top: -1px;width: 50px;padding-top: 3px;'>" + enddate + "</div>");
+                                $('.ul_one').append("<div class='fixuxian' style='position: absolute;top: 32px;left: 0;height: 160px;border-right: 1px dotted rgb(138, 131, 131);'></div>");
+                                $('.ul_one').append("<div class='fidate' style='position: absolute;top: -1px;padding-top: 15px;width: 56px;'>" + enddate + "</div>");
                                 $('.fixuxian').css('width', divwidth);
-                                $('.fidate').css('left', divwidth);
+                                $('.fidate').css('left', divwidth - 19);
                             }
 
                         }
 
-
+                        console.log(li_num);//li_num=[0,1,2]
+                        var all_li = ['0','1','2','3','4'];
+                        var diff = li_num.concat(all_li).filter(v => !li_num.includes(v) || !all_li.includes(v));//取差集
+                        console.log(diff);
+                        
                         totalWeek = parseInt(totalWeek + 0.99);
 
                         for (var i = 1; i <= totalWeek; i++) {
@@ -193,6 +176,6 @@
                     })
                 }
             })
-</script>
-</body>
+        </script>
+    </body>
 </html>
